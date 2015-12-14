d0234bc31f7e7cc3e8ae7663e49a237faeed20d5
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-14 17:27:09 +0530

    Fixes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
   end
 
   def search_the_crap_out_of_redis
+    SearchBot.new(params["query"]).search_in_code_base
     render nothing: true, status: :ok, content_type: "text/html"
   end
 
--- app/services/search_bot.rb
+++ app/services/search_bot.rb
@@ -1,12 +1,13 @@
 class SearchBot
 
-  def def initialize(query)
+  def initialize(query)
     @query = query
     @redis_code_base_path = Rails.public_path + 'redis-unstable'
     @directories_to_search_through = ['deps', 'src', 'tests', 'utils']
   end
 
   def search_in_code_base
+    binding.pry
   end
 
 end
