282eb7fcfb76e1e69fa21a74d5faaf87fc4b9ccf
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-15 10:37:20 +0530

    Added directories to traverse

--- app/services/search_bot.rb
+++ app/services/search_bot.rb
@@ -3,11 +3,15 @@ class SearchBot
   def initialize(query)
     @query = query
     @redis_code_base_path = Rails.public_path + 'redis-unstable'
-    @directories_to_search_through = ['deps', 'src', 'tests', 'utils']
+    @sub_directories_to_search_through = ['deps', 'src', 'tests', 'utils']
   end
 
   def search_in_code_base
-    binding.pry
+    @sub_directories_to_search_through.each do |sub_directory|
+      binding.pry
+      _directory_path = @redis_code_base_path + sub_directory
+
+    end
   end
 
 end
